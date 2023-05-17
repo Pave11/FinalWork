e215407831f9d1a571b9d160b9ad036eaec0cc76
Pavel <perkin.pavel04@gmail.com>
2023-05-17 17:25:30 +0300

    change PROG

--- LR10/LR10/main.c
+++ LR10/LR10/main.c
@@ -5,34 +5,34 @@
 
 int main() {
 	FILE* a = fopen("a.txt", "w");
-	float sr = 0;
-	char g[100];
-	int m[10], m1[10], sum = 0, el;
+	float sr1 = 0;
+	char g1[100];
+	int m1[10], m2[10], sum1 = 0, el;
 	for (int i = 0; i < 10; i++) {
-		m[i] = rand() % 100;
-		fprintf(a, " %d\n", m[i]);
+		m1[i] = rand() % 100;
+		fprintf(a, " %d\n", m1[i]);
 	}
 	fprintf(a, " \n");
 	fclose(a);
 	a = fopen("a.txt", "r");
 	for (int i = 0; i < 10; i++) {
-		fgets(g, 100, a);
-		printf("m[%d]:%s", i, g);
+		fgets(g1, 100, a);
+		printf("m[%d]:%s", i, g1);
 	}
 	fclose(a);
 	a = fopen("a.txt", "r");
-	int* pM1 = &m1[0];
+	int* pM1 = &m2[0];
 	for (int i = 0; i < 10; i++) {
 		fscanf(a, "%d", &pM1);
 		el = pM1;
-		sum += el;
+		sum1 += el;
 		pM1++;
 	}
 
 	FILE* b = fopen("b.txt", "w");
-	sr = (float)((float)sum / 10);
-	fprintf(b, "%fl\n", sr);
+	sr1 = (float)((float)sum1 / 10);
+	fprintf(b, "%fl\n", sr1);
 	fclose(b);
-	printf("Otvet: %4.1f", sr);
+	printf("Otvet: %4.1f", sr1);
 	getch();
 }
